6601b6675e096958911e87ebf8d1ccc58066155a
Los Tapias <lostapias@Loss-MacBook-Pro.local>
2019-11-04 10:56:37 -0800

    Added Game-Images

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
-console.log('hi')
+class Player{
+	constructor(){
+		this.HP = 100,
+		this.damage = Math.floor((Math.random()*16)+5),
+		this.ammo = 100
+
+	}
+}
+
+class Enemy{
+	constructor(){
+		this.HP = 25
+	}
+}
+
+const druggie = new Player()
+let bagOfChips = new Enemy()
+console.log(druggie)
