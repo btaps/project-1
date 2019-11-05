ffcdf1122fd2f2e509a908a5d681f174c3536240
Los Tapias <lostapias@Loss-MacBook-Pro.local>
2019-11-04 16:57:21 -0800

    Added CSS and Images

--- app.js
+++ app.js
@@ -2,8 +2,8 @@ class Player{
 	constructor(){
 		this.HP = 100,
 		this.damage = Math.floor((Math.random()*16)+5),
-		this.ammo = 100
-
+		this.ammo = 100,
+		this.weapon = 'gun'
 	}
 }
 
@@ -13,6 +13,37 @@ class Enemy{
 	}
 }
 
-const druggie = new Player()
-let bagOfChips = new Enemy()
-console.log(druggie)
+const druggie = new Player();
+const hotdog = new Enemy();
+const cookie = new Enemy();
+const pizza = new Enemy();
+
+let shooter = document.querySelector('.player')
+let keyLeft = false
+let keyRight = false
+
+const keyDown = (e)=>{
+	if(e.keycode == 65){
+		console.log('left')
+		keyLeft = true;
+	}else if(e.keycode == 68){
+		console.log('right')
+		keyRight = true;
+	}
+}
+
+let keyUp = (e)=>{
+	if(e.keycode == 65){
+		keyLeft = false;
+	}else if(e.keycode == 68){
+		keyRight = false;
+	}
+}
+
+
+/*.  This grabs css properties for element
+let game = document.querySelector('.gameScreen')
+window.getComputedStyle(game).width
+
+*/
+
